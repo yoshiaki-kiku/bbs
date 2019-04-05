91c52f7e605b0208486507f7978c9756d3dce419
yoshiaki-kiku <yoshiworks333@gmail.com>
2019-04-05 21:08:24 +0900

    commentsマイグレーションの追加
    カラムの追加

--- database/migrations/2019_04_05_120359_add_column_comments_table.php
+++ database/migrations/2019_04_05_120359_add_column_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
+class AddColumnCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('topic_id')->unsigned();
+            $table->integer('response_id')->unsigned();
+            $table->text('message');
+            $table->integer('votes');
         });
     }
 
@@ -26,6 +28,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        //
     }
 }
