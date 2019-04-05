bc1d36fca0621a965be387b14ef504b6791558c8
yoshiaki-kiku <yoshiworks333@gmail.com>
2019-04-05 20:58:02 +0900

    ファーストコミット

---
+++ database/migrations/2019_04_05_112938_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('topic_id')->unsigned();
+            $table->integer('response_id')->unsigned();
+            $table->text('message');
+            $table->integer('votes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
