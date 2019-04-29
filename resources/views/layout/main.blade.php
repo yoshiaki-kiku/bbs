2a2b1c433bffacc32df0f3ce8373d0e158f19a55
yoshiaki-kiku <yoshiworks333@gmail.com>
2019-04-29 11:57:27 +0900

    metaにnofollow追加

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="robots" content="noindex,nofollow">
     <title>交流サイト@hasSection('title') - @yield('title') @endif</title>
     @include("layout.styles")
     <style type="text/css">
